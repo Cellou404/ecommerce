3cd32136c2fa477e73364f6bf89d2989b4016ecd
Cellou Cisse <cellou649@gmail.com>
2025-01-26 20:39:59 +0000

    format code

--- products/views.py
+++ products/views.py
@@ -11,10 +11,13 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request,
-                 'products/list.html',
-                 {'category': category,
-                  'categories': categories,
-                  'products': products})
+        'products/list.html',
+        {
+            'category': category,
+            'categories': categories,
+            'products': products
+        }
+    )
 
 def product_detail(request, category_slug, slug):
     category = get_object_or_404(Category, slug=category_slug)
